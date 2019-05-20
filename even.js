44995d892f8eae6782905d0b789ac19f0bf023f6
Olanrewaju Afolayan <michaelflourish@hotmail.com>
2019-05-20 17:14:22 +0000

    even.js exercise with 'isEven' function completed

--- even.js
+++ even.js
@@ -2,5 +2,8 @@ function isEven(num) {
   return num % 2 === 0;
 }
 
-console.log(isEven(10));
-console.log(isEven(11));
+var tenIsEven = isEven(10);
+var elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
