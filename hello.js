abb61650660d3d039bb0be6153a3d054d7e8ef8e
Olanrewaju Afolayan <michaelflourish@hotmail.com>
2019-05-20 17:08:04 +0000

    hello.js exercise with function

--- hello.js
+++ hello.js
@@ -2,6 +2,20 @@ function sayHello(name) {
   console.log("Hello World, " + name);
 }
 
-sayHello("Flourish");
-sayHello("Michael");
-sayHello("Micah");
+//sayHello("Flourish");
+//sayHello("Michael");
+//sayHello("Micah");
+
+function sayHelloToConsole(name) {
+  console.log("Hello, " + name);
+
+}
+
+sayHelloToConsole("Flourish");
+
+function returnSayHello(name) {
+  return "Hello, " + name;
+}
+
+var greeting = returnSayHello("John");
+console.log(greeting);
